8828724aad6c3788c937e5fd379dc5eda89e7a35
Anirudh Kulkarni <askakdagr8@gmail.com>
2022-10-29 21:08:44 -0500

    Finished w/ piggy

--- Piggy/Pig.gd
+++ Piggy/Pig.gd
@@ -2,13 +2,19 @@ extends Area2D
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-const SPEED = 200
+export(int) var SPEED = 200
+
+onready var animationPlayer = $AnimationPlayer
+onready var sprite = $Sprite
+
 
 func _process(delta):
 	if Input.is_action_pressed("ui_left"):
 		move(-SPEED,0,delta)
+		sprite.flip_h = true
 	if Input.is_action_pressed("ui_right"):
 		move(SPEED,0,delta)
+		sprite.flip_h = false
 	if Input.is_action_pressed("ui_up"):
 		move(0,-SPEED,delta)
 	if Input.is_action_pressed("ui_down"):
@@ -17,4 +23,10 @@ func _process(delta):
 func move(x,y,delta):
 	position.x += (x*delta)
 	position.y += (y*delta)
-	
+	animationPlayer.play("Run")
+
+
+
+func _on_Pig_area_entered(area):
+	area.queue_free()
+	scale *= 1.1
